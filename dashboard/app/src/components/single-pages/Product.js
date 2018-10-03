2486b0d606bd2197c5b32cf35b519d8128bd465d
pichuca <info@pichucasystems.com>
2018-10-03 01:50:34 -0300

    Work on product image page.

--- dashboard/app/src/components/single-pages/Product.js
+++ dashboard/app/src/components/single-pages/Product.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Product.css';
 
 const Product = ({ match }) => (
-  <div>
+  <div className="product">
+    <div className="image-wrapper">
+      <img src="" alt="" />
+    </div>
     <h3>SKU #: {match.params.sku}</h3>
-    <h3>{match.params.name}</h3>
+    <h3>nombre de producto: {match.params.name}</h3>
     <h3>Disponibilidad: {match.params.availability}</h3>
   </div>
 );
