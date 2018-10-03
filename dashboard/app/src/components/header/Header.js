22ac54379d8b779634470a2f9540f309d4df832c
pichuca <info@pichucasystems.com>
2018-10-03 01:01:10 -0300

    Added selected status for nav links.

--- dashboard/app/src/components/header/Header.js
+++ dashboard/app/src/components/header/Header.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link, location } from "react-router-dom";
 
 import './Header.css';
 
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectedLink: null,
-    };
+    this.state = {};
+    this.browserUrlIncludes = this.browserUrlIncludes.bind(this);
   }
-  handleLinkClick(event) {
-    console.log(event.currentTarget.id);
-    let clickedId = event.currentTarget.id;
-    this.setState({
-      selectedLink: clickedId,
-    });
+  browserUrlIncludes(match) {
+    return window.location.pathname.includes(match);
   }
   render() {
     const selectedLink = this.state.selectedLink;
@@ -25,11 +20,11 @@ class Header extends Component {
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <nav>
             <div className="links">
-                <Link id="inventory" className={selectedLink === this.id ? 'selected' : ''} to="/inventory" onClick={this.handleLinkClick.bind(this)}>Inventario</Link>
-                <Link id="orders" className={selectedLink === this.id ? 'selected' : ''} to="/orders" onClick={this.handleLinkClick.bind(this)}>Pedidos</Link>
-                <Link id="entries" className={selectedLink === this.id ? 'selected' : ''} to="/entries" onClick={this.handleLinkClick.bind(this)}>Solicitudes</Link>
-                <Link id="stats" className={selectedLink === this.id ? 'selected' : ''} to="/stats" onClick={this.handleLinkClick.bind(this)}>Estadísticas</Link>
-                <Link id="configuration" className={selectedLink === this.id ? 'selected' : ''} to="/configuration" onClick={this.handleLinkClick.bind(this)}>Configuración</Link>
+                <Link id="inventory" className={this.browserUrlIncludes('/inventory') ? 'selected' : ''} to="/inventory">Inventario</Link>
+                <Link id="orders" className={this.browserUrlIncludes('/orders') ? 'selected' : ''} to="/orders">Pedidos</Link>
+                <Link id="entries" className={this.browserUrlIncludes('/entries') ? 'selected' : ''} to="/entries">Solicitudes</Link>
+                <Link id="stats" className={this.browserUrlIncludes('/stats') ? 'selected' : ''} to="/stats">Estadísticas</Link>
+                <Link id="configuration" className={this.browserUrlIncludes('/configuration') ? 'selected' : ''} to="/configuration">Configuración</Link>
             </div>
             </nav>
         </header>
