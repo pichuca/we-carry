7b010ec53bd96ee88154ead437993270f719dbfb
pichuca <info@pichucasystems.com>
2018-10-02 23:42:37 -0300

    Working on tables. Intalled dependencies.

--- dashboard/app/src/components/header/Header.js
+++ dashboard/app/src/components/header/Header.js
@@ -4,18 +4,32 @@ import { Link } from "react-router-dom";
 import './Header.css';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLink: null,
+    };
+  }
+  handleLinkClick(event) {
+    console.log(event.currentTarget.id);
+    let clickedId = event.currentTarget.id;
+    this.setState({
+      selectedLink: clickedId,
+    });
+  }
   render() {
+    const selectedLink = this.state.selectedLink;
     return (
     <React.Fragment>
         <header className="App-header">
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <nav>
             <div className="links">
-                <Link to="/inventory">Inventario</Link>
-                <Link to="/orders">Pedidos</Link>
-                <Link to="/entries">Solicitudes</Link>
-                <Link to="/stats">Estadísticas</Link>
-                <Link to="/configuration">Configuración</Link>
+                <Link id="inventory" className={selectedLink === this.id ? 'selected' : ''} to="/inventory" onClick={this.handleLinkClick.bind(this)}>Inventario</Link>
+                <Link id="orders" className={selectedLink === this.id ? 'selected' : ''} to="/orders" onClick={this.handleLinkClick.bind(this)}>Pedidos</Link>
+                <Link id="entries" className={selectedLink === this.id ? 'selected' : ''} to="/entries" onClick={this.handleLinkClick.bind(this)}>Solicitudes</Link>
+                <Link id="stats" className={selectedLink === this.id ? 'selected' : ''} to="/stats" onClick={this.handleLinkClick.bind(this)}>Estadísticas</Link>
+                <Link id="configuration" className={selectedLink === this.id ? 'selected' : ''} to="/configuration" onClick={this.handleLinkClick.bind(this)}>Configuración</Link>
             </div>
             </nav>
         </header>
--- dashboard/app/src/components/sections/inventory/Inventory.js
+++ dashboard/app/src/components/sections/inventory/Inventory.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SimpleTable from '../../../components/table/SimpleTable';
 
 class Inventory extends Component {
   render() {
     return (
       <React.Fragment>
         <h1>Inventory</h1>
+        <SimpleTable />
       </React.Fragment>
     );
   }
