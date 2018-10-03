5999b9057773ad61746c1c19d5600af995aee643
pichuca <info@pichucasystems.com>
2018-10-03 11:15:32 -0300

    Added form ui.

--- dashboard/app/src/components/single-pages/entry/Entry.js
+++ dashboard/app/src/components/single-pages/entry/Entry.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import Form from '../../form/Form';
 
 import './Entry.css';
 
@@ -6,6 +9,12 @@ const Entry = () => (
   <div className="entry">
     <h2>Pedido de ingreso nuevo:</h2>
     <h4>Completa los datos para realizar tu pedido de nuevo ingreso de mercadería.</h4>
+    <Card className="entry-form-wrapper">
+      <Form />
+      <Button variant="outlined">
+          Ingresar
+      </Button>
+    </Card>
   </div>
 );
 
