aaececd7e7d2d2f95d271e2a9a76442b71682c53
pichuca <info@pichucasystems.com>
2018-10-30 15:48:30 -0300

    Work on styles for entries.

--- dashboard/app/src/components/sections/entries/Entries.js
+++ dashboard/app/src/components/sections/entries/Entries.js
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import Table from '../../table/EnancedTable.entries';
 
+import './Entries.css';
+
 class Entries extends Component {
   render() {
     return (
-      <React.Fragment>
+      <div className="entries">
         <h1>Ingresos de mercadería</h1>
         <Link to="/entry">
-          <Button className="entry-button">Nuevo ingreso</Button>
+          <Button variant="contained" className="entry-button">Nuevo ingreso</Button>
         </Link>
         <Table />
-      </React.Fragment>
+      </div>
     );
   }
 }
