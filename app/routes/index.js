6417c3b7219f285614acdfd72bc87adfde71d4d8
pichuca <info@pichucasystems.com>
2019-03-29 18:34:06 -0300

    Added request npm pack

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
+var request = require('request');
 const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome Page
 router.get('/', (req, res) => res.render('welcome'));
 
 // Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) => 
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    const url = 'http://190.221.30.210/intranet/ServiciosWeb/ConsultaStock.asmx?op=obtenerStock';
+    const data = request(url, (err, res, body) => {
+        console.log('error:', err); // Print the error if one occurred
+        console.log('statusCode:', res && res.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        return body;
+    });
     res.render('dashboard', {
         user: {
             name: req.user.name
-        }
-    }));
+        },
+        products: data
+    });
+});
 
 router.get('/orders', ensureAuthenticated, (req, res) =>
     res.render('orders'));
