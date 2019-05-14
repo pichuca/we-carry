2b696740c01c0c94685f0c1735b51f2aca41594a
pichuca <info@pichucasystems.com>
2019-05-14 18:23:53 -0300

    Work on tracking page

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
+const moment = require('moment');
+
+// moment
+moment().locale('es');
+console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
 // Product page
 router.get('/:id/:name/:availability/:ordered', ensureAuthenticated, (req, res) => 
@@ -14,10 +20,12 @@ router.get('/:id/:name/:availability/:ordered', ensureAuthenticated, (req, res) 
         }
     }));
 
-router.get('/tracking/:id', ensureAuthenticated, (req, res) =>
+router.get('/tracking/:id', ensureAuthenticated, (req, res) => 
     res.render('tracking', {
         product: {
-            id: req.params.id
+            id: req.params.id,
+            date: currentDate
+            // TODO: add more info: image, etc.
         }
     }));
 
