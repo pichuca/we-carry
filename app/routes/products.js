d7e0f99a1da9cc7541425a88f065a977fe9ecec4
pichuca <info@pichucasystems.com>
2019-05-15 11:09:28 -0300

    Work on product single page

--- app/routes/products.js
+++ app/routes/products.js
@@ -9,7 +9,9 @@ router.get('/:id/:name/:availability/:ordered', ensureAuthenticated, (req, res) 
             id: req.params.id,
             name: req.params.name,
             availability: req.params.availability,
-            ordered: req.params.ordered
+            ordered: req.params.ordered,
+            description: 'gorras de golf marca Taylormade.', // TODO: Get dynamic product description
+            stock: true ? 'Cuenta con stock disponible.' : 'No hay stock en este momento.' // Get dynamic stock
             // TODO: add image, or whatever fields that want to be added to this route
         }
     }));
