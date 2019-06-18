f934806a522281d38a4c6074b7c7f806a7d0262c
pichuca <info@pichucasystems.com>
2019-06-17 23:58:42 -0300

    Added warehouse stock dynamically

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,82 +3,10 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 // const request = require('request');
-// const soapRequest = require('../modules/soapRequest.js');
+const storage = require('node-persist');
 const soapRequest = require('easy-soap-request');
 const parseString = require('xml2js').parseString;
 const { ensureAuthenticated } = require('../config/auth');
-
-
-/**
- * SOAP data
- */
-const getStock = (sessionXML) => {
-    console.log(`Get stock function called`);
-    const requestData = {
-        url: 'http://190.221.30.210/intranet/ServiciosWeb/consultaStock.asmx',
-        headers: {
-            'Content-Type': 'text/xml;charset=UTF-8',
-            'soapAction': 'http://brainsys.com.ar/obtenerStock',
-        },
-        xml: fs.readFileSync('xml/brainsys-services/ConsultaStockRequest.xml'),
-    };
-
-    (async () => {
-        console.log('SOAP obtenerStock async function triggered');
-        try {
-            const { response } = await soapRequest(requestData.url, requestData.headers, requestData.xml, 100000); // Stock needs bigger timeouts
-            const { body, statusCode } = response;
-            console.log(`Request finished with status code: ${statusCode}`);
-            console.log('Stock fetched as XML...');
-            parseString(body, function (err, result) {
-                if (err) {
-                    console.log(`Error on parsing XML response for authentication: ${err}`);
-                } else {
-                    console.log('XML converted to JSON:');
-                    console.log(result['soap:Envelope']['soap:Body'][0].obtenerStockResponse[0].obtenerStockResult[0].Stock);
-                    let stockArray = result['soap:Envelope']['soap:Body'][0].obtenerStockResponse[0].obtenerStockResult[0].Stock;
-                    console.log(`Stock array length: ${stockArray.length}`);
-                }
-            });
-        } catch(err) {
-            console.log(`SOAP ERROR on async function: ${err}`);
-        }
-    })();
-};
-
-const soapAuthenticationData = {
-    url: 'http://190.221.30.210/intranet/ServiciosWeb/autenticacion.asmx',
-    headers: {
-        'Content-Type': 'text/xml;charset=UTF-8',
-        'soapAction': 'http://brainsys.com.ar/autenticar',
-    },
-    xml: fs.readFileSync('xml/brainsys-services/AutenticacionRequest.xml', 'utf-8'),
-};
-
-const soapAuthentication = () => {
-    // usage of module
-    (async () => {
-        console.log('SOAP authentication async function triggered');
-        try {
-            const { response } = await soapRequest(soapAuthenticationData.url, soapAuthenticationData.headers, soapAuthenticationData.xml, 10000); // Optional timeout parameter(milliseconds)
-            const { body, statusCode } = response;
-            console.log(body);
-            console.log(statusCode);
-            if (!body) {
-                console.warn('No body for session object');
-            } else {
-                console.log('Fetching stock');
-                getStock(body);
-            }
-        } catch(err) {
-            console.log(`SOAP ERROR on async function: ${err}`);
-        }
-    })();
-};
-
-/**
- * End SOAP data
- */
 
 // Welcome Page
 // TODO: should add welcome view if want to re add register option
@@ -86,15 +14,79 @@ router.get('/', (req, res) => res.render('login'));
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-
-    // TODO: get object for stock requests.
-    soapAuthentication();
-
     res.render('dashboard', {
         user: {
             name: req.user.name
         }
     });
+});
+
+router.get('/dashboard/getStock', ensureAuthenticated, (req, res) => {
+        // Get stock function
+        const getStock = () => {
+            console.log(`Get stock function called`);
+            const requestData = {
+                url: 'http://190.221.30.210/intranet/ServiciosWeb/consultaStock.asmx',
+                headers: {
+                    'Content-Type': 'text/xml;charset=UTF-8',
+                    'soapAction': 'http://brainsys.com.ar/obtenerStock',
+                },
+                xml: fs.readFileSync('xml/brainsys-services/ConsultaStockRequest.xml'),
+            };
+        
+            (async () => {
+                try {
+                    const { response } = await soapRequest(requestData.url, requestData.headers, requestData.xml, 100000); // Stock needs bigger timeouts
+                    const { body, statusCode } = response;
+                    console.log(`Request finished with status code: ${statusCode}`);
+                    console.log('Stock fetched as XML...');
+                    parseString(body, function (err, result) {
+                        if (err) {
+                            console.log(`Error on parsing XML response for authentication: ${err}`);
+                        } else {
+                            console.log('XML converted to JSON:');
+                            console.log(result['soap:Envelope']['soap:Body'][0].obtenerStockResponse[0].obtenerStockResult[0].Stock);
+                            let stockArray = result['soap:Envelope']['soap:Body'][0].obtenerStockResponse[0].obtenerStockResult[0].Stock;
+                            console.log(`Stock array length: ${stockArray.length}`);
+                            res.send(stockArray);
+                        }
+                    });
+                } catch(err) {
+                    console.log(`SOAP ERROR on async function: ${err}`);
+                }
+            })();
+        };
+        // SOAP Authentication
+        /**
+         * SOAP data
+         */
+        const soapAuthenticationData = {
+            url: 'http://190.221.30.210/intranet/ServiciosWeb/autenticacion.asmx',
+            headers: {
+                'Content-Type': 'text/xml;charset=UTF-8',
+                'soapAction': 'http://brainsys.com.ar/autenticar',
+            },
+            xml: fs.readFileSync('xml/brainsys-services/AutenticacionRequest.xml', 'utf-8'),
+        };
+        /**
+         * End SOAP data
+         */
+        (async () => {
+            try {
+                const { response } = await soapRequest(soapAuthenticationData.url, soapAuthenticationData.headers, soapAuthenticationData.xml, 10000); // Optional timeout parameter(milliseconds)
+                const { body, statusCode } = response;
+                console.log(body);
+                console.log(statusCode);
+                if (!body) {
+                    console.warn('No body for session object');
+                } else {
+                    console.log('Fetching stock');
+                    getStock(body);
+                }
+            } catch(err) {
+                console.log(`SOAP ERROR on async Auth function: ${err}`);
+            }
+        })();
 });
 
 router.get('/orders', ensureAuthenticated, (req, res) =>
--- app/routes/products.js
+++ app/routes/products.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
 // Product page
-router.get('/:id/:name/:availability/:ordered', ensureAuthenticated, (req, res) => 
+router.get('/:id/:name/:availability', ensureAuthenticated, (req, res) => 
     res.render('product', {
         product: {
             id: req.params.id,
             name: req.params.name,
             availability: req.params.availability,
             ordered: req.params.ordered,
-            description: 'gorras de golf marca Taylormade.', // TODO: Get dynamic product description
-            stock: true ? 'Cuenta con stock disponible.' : 'No hay stock en este momento.' // Get dynamic stock
+            description: 'Descripcion no disponible.', // TODO: Get dynamic product description
+            stock: req.params.availability > 0 ? 'Cuenta con stock disponible.' : 'No hay stock en este momento.' // Get dynamic stock
             // TODO: add image, or whatever fields that want to be added to this route
         }
     }));
